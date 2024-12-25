89a41b73215f167799f7e3bfc5c513f2194b6704
userlanats <lanatsamalashvili@gmail.com>
2024-12-25 18:04:17 +0400

    change message

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -63,7 +63,7 @@ const Contact = () => {
             for="phonenumber"
             class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
-            Your email
+            Your phone number
           </label>
           <input
             type="number"
